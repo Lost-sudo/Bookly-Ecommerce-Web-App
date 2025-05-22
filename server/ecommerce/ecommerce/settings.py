6091e0ddcfb88ced5df4492582a9ce28a5e37323
John Patrick Paraon <patrickparaon041@gmail.com>
2025-05-22 21:44:07 +0800

    fix: Register your_app in INSTALLED_APPS and update admin creation logic

--- server/ecommerce/ecommerce/settings.py
+++ server/ecommerce/ecommerce/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'orders',
+    'your_app.apps.YourAppConfig',
 ]
 
 MIDDLEWARE = [
--- server/ecommerce/your_app/apps.py
+++ server/ecommerce/your_app/apps.py
@@ -1,8 +1,8 @@
 from django.apps import AppConfig
-from .create_admin import run as create_admin
 
 class YourAppConfig(AppConfig):
     name = 'your_app'
 
     def ready(self):
-        create_admin()
+        from .create_admin import run
+        run()
