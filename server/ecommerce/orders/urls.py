4e0f12b86797ebf127e0ab2234d15a5fb25aadc3
John Patrick Paraon <patrickparaon041@gmail.com>
2025-05-23 21:35:42 +0800

    refactor: Update orders URL configuration for consistency in basename

--- server/ecommerce/orders/urls.py
+++ server/ecommerce/orders/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import OrderViewSet
 
 router = DefaultRouter()
-router.register(r'orders', OrderViewSet, basename='orders')
+router.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [
     path('api/', include(router.urls)),
