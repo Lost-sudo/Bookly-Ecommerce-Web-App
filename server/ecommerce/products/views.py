a42c19b40bfd75e98eae5015b95336215956bace
John Patrick Paraon <patrickparaon041@gmail.com>
2025-05-23 16:41:15 +0800

    refactor: Remove sample products fallback from BookViewSet list method

--- server/ecommerce/products/views.py
+++ server/ecommerce/products/views.py
@@ -19,51 +19,6 @@ class BookViewSet(viewsets.ReadOnlyModelViewSet):
     filterset_class = BookFilter
     search_fields = ['title', 'author', 'description']
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        if not queryset.exists():
-            # --- Sample products fallback ---
-            sample_products = [
-                {
-                    "id": 1001,
-                    "title": "The Pragmatic Programmer",
-                    "author": "Andrew Hunt, David Thomas",
-                    "description": "A classic book for software engineers.",
-                    "price": "1200.00",
-                    "category": "Programming",
-                    "genre": "Technology",
-                    "sub_genre": "Software Engineering",
-                    "cover_image": "https://pragprog.com/titles/tpp20/the-pragmatic-programmer-20th-anniversary-edition/tpp20.jpg"
-                },
-                {
-                    "id": 1002,
-                    "title": "z ",
-                    "author": "Robert C. Martin",
-                    "description": "A Handbook of Agile Software Craftsmanship.",
-                    "price": "950.00",
-                    "category": "Programming",
-                    "genre": "Technology",
-                    "sub_genre": "Software Engineering",
-                    "cover_image": "blob:https://www.kobo.com/86cf7011-a17c-4306-bea5-1886a41f7609"
-                },
-                {
-                    "id": 1003,
-                    "title": "Atomic Habits",
-                    "author": "James Clear",
-                    "description": "An Easy & Proven Way to Build Good Habits & Break Bad Ones.",
-                    "price": "800.00",
-                    "category": "Self-Help",
-                    "genre": "Nonfiction",
-                    "sub_genre": "Personal Development",
-                    "cover_image": "blob:https://www.kobo.com/7d92cbd0-77bd-4e73-8d28-78d62cf0cb64"
-                },
-                # ...add more sample products as needed...
-            ]
-            # Use serializer to keep response format consistent
-            serializer = self.get_serializer(sample_products, many=True)
-            return Response(serializer.data)
-        return super().list(request, *args, **kwargs)
-
     @action(detail=False, methods=['get'], url_path='trending')
     def trending_books(self, request):
         queryset = Book.objects.filter(category__isnull=False).order_by('-created_at')[:10]
