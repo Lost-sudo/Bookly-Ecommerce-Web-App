5d355cdbb6143bc67a35ee2a5a8c5ccf70524836
John Patrick Paraon <patrickparaon041@gmail.com>
2025-05-23 18:10:21 +0800

    feat: Refactor BookCustomerSerializer to use SerializerMethodField for genre, sub_genre, category, and cover_image

--- server/ecommerce/products/serializers.py
+++ server/ecommerce/products/serializers.py
@@ -10,12 +10,30 @@ class BookAdminSerializer(serializers.ModelSerializer):
         read_only_fields = ['id']
 
 class BookCustomerSerializer(serializers.ModelSerializer):
-    genre = serializers.StringRelatedField()
-    sub_genre = serializers.StringRelatedField()
-    category = serializers.StringRelatedField()
-    cover_image = serializers.ImageField(read_only=True)
+    genre = serializers.SerializerMethodField()
+    sub_genre = serializers.SerializerMethodField()
+    category = serializers.SerializerMethodField()
+    cover_image = serializers.SerializerMethodField()
 
     class Meta:
         model = Book
         fields = ['id', 'title', 'author', 'description', 'price', 'category', 'genre', 'sub_genre', 'cover_image']
         read_only_fields = ['id']
+
+    def get_genre(self, obj):
+        return obj.genre.name if obj.genre else None
+
+    def get_sub_genre(self, obj):
+        return obj.sub_genre.name if obj.sub_genre else None
+
+    def get_category(self, obj):
+        return obj.category.name if obj.category else None
+
+    def get_cover_image(self, obj):
+        # Return the URL if cover_image exists, else None
+        if obj.cover_image:
+            try:
+                return obj.cover_image.url
+            except Exception:
+                return None
+        return None
